96f02d00708872f47f6f4ed114847218b13a7453
Connor Urbano Mendoza <134821826+Connor-UM-18@users.noreply.github.com>
2025-04-08 22:33:14 -0600

    Subicion

---
+++ Tarea4_2/app/src/main/java/com/example/tarea4_2/GoogleMapsFragmentWrapper.kt
@@ -0,0 +1,26 @@
+package com.example.tarea4_2
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.NonNull
+import androidx.annotation.Nullable
+import androidx.fragment.app.Fragment
+
+class GoogleMapsFragmentWrapper : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Start the GoogleMapsActivity
+        val intent = Intent(activity, GoogleMapsActivity::class.java)
+        startActivity(intent)
+
+        // Return a placeholder view (important to prevent errors)
+        return View(context)
+    }
+}
